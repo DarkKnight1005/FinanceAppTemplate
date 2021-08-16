6f20ec110246e38681a1e43ad71575ae9f027baf
DarkKnight1005 <bloodykillerplay@gmail.com>
2021-08-16 15:18:35 +0400

    Initial Commit

---
+++ FinanceAppTemplate/FinanceAppTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceAppTemplateApp.swift
+//  FinanceAppTemplate
+//
+//  Created by Ayaz Panahov on 16.08.21.
+//
+
+import SwiftUI
+
+@main
+struct FinanceAppTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
